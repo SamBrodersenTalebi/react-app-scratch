0f9c27f322031a567825a01a223d91ee63857bf4
Sam Brodersen <sam.talebi84@gmail.com>
2020-05-02 16:22:36 +0200

    add webpack server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 
-const App = () => <div className='container'>hello webpack</div>;
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const [values, setValues] = useState([]);
+
+  const handleClick = () => {
+    setCounter(counter + 1);
+    setValues(values.concat(counter));
+  };
+
+  return (
+    <div className='container'>
+      hello webpack {counter} clicks
+      <button onClick={handleClick}>press</button>
+    </div>
+  );
+};
 
 export default App;
